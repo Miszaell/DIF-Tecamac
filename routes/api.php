db742235021e8ee7a06a3afcb1ab30a8c88d56f5
Miszaell <misaelvgm011@gmail.com>
2021-12-22 17:47:44 -0600

    add routes || add api auth

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\ComunicationController;
 use App\Http\Controllers\DocumentController;
+use App\Http\Controllers\ImageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,11 +16,49 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post("test", [ComunicationController::class, 'delete']);
-Route::post("testDoc", [DocumentController::class, 'delete']);
+Route::group([
+    'prefix' => 'auth',
+], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('signup', [AuthController::class, 'signUp']);
+    Route::get('logout', [AuthController::class,'logout'])->middleware('auth:api');
+});
+
+Route::group([
+    'prefix' => 'admin/comunicacion',
+    'middleware' => 'auth:api',
+], function () {
+    Route::get("getAll", [ComunicationController::class, 'get']);
+    Route::get("getById", [ComunicationController::class, 'getById']);
+    Route::post("post", [ComunicationController::class, 'post']);
+    Route::put("put", [ComunicationController::class, 'put']);
+    Route::delete("delete", [ComunicationController::class, 'delete']);
+});
+
+Route::group([
+    'prefix' => 'admin/documents',
+    'middleware' => 'auth:api',
+], function () {
+    Route::get("getAll", [DocumentController::class, 'get']);
+    Route::get("getById", [DocumentController::class, 'getById']);
+    Route::post("post", [DocumentController::class, 'post']);
+    Route::put("put", [DocumentController::class, 'put']);
+    Route::delete("delete", [DocumentController::class, 'delete']);
+});
+
+Route::group([
+    'prefix' => 'admin/images',
+    'middleware' => 'auth:api',
+], function () {
+    Route::get("getAll", [ImageController::class, 'get']);
+    Route::get("getById", [ImageController::class, 'getById']);
+    Route::post("post", [ImageController::class, 'post']);
+    Route::put("put", [ImageController::class, 'put']);
+    Route::delete("delete", [ImageController::class, 'delete']);
+});
